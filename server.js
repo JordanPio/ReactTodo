c4256539877a7fcf8916c6cd9df60a32f5a258fa
Jordan Pio <czpjunior@gmail.com>
2020-03-30 07:33:41 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const path = require("path");
+const morgan = require("morgan");
+const compression = require("compression");
+const dev = app.get("env") !== "production";
+
+// load dotenv and config port
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+port = process.env.PORT;
+app.listen(port, () => console.log(`server started on port ${port}`));
+
+if (!dev) {
+  app.disable("x-powered-by");
+  app.use(compression());
+  app.use(morgan("common"));
+
+  app.use(express.static(path.resolve(__dirname, "build")));
+
+  app.get("*", (req, res) => {
+    res.sendfile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
+if (dev) {
+  app.use(morgan("dev"));
+}
+
+// router
+
+const server = require("http").createServer(app);
+
+//export server
+
+module.exports = server;
